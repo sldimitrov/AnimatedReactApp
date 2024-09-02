c936f48324384430798c342766e72d7dbcf22781
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-02 16:01:24 +0300

    Scroll-based aniamtions

--- src/pages/Challenges.jsx
+++ src/pages/Challenges.jsx
@@ -1,6 +1,6 @@
-import Header from '../components/Header.jsx';
-import Challenges from '../components/Challenges.jsx';
-import ChallengesContextProvider from '../store/challenges-context.jsx';
+import Header from "../components/Header.jsx";
+import Challenges from "../components/Challenges.jsx";
+import ChallengesContextProvider from "../store/challenges-context.jsx";
 
 export default function ChallengesPage() {
   return (
